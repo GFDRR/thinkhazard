e771aac65ab5127e940972b6c4c78a22895bb6de
Éric Lemoine <eric.lemoine@gmail.com>
2015-03-12 11:29:32 +0100

    Add debug mode to page
    This commit makes it possible to load the page in debug mode, using `?debug=` in the page URL. In this mode the CSS and JS code is not minified.

--- thinkhazard/views.py
+++ thinkhazard/views.py
@@ -3,5 +3,7 @@ from pyramid.view import view_config
 
 @view_config(route_name='index', renderer='templates/index.jinja2')
 def index(request):
+    debug = 'debug' in request.params
     settings = request.registry.settings
-    return {'node_modules': settings.get('node_modules')}
+    return {'debug': debug,
+            'node_modules': settings.get('node_modules')}
