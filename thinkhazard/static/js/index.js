881355acb8f57ce1bca02833b2b1c14375adcedc
Éric Lemoine <eric.lemoine@gmail.com>
2015-03-12 11:58:08 +0100

    Add a typeahead-based search field to index page

---
+++ thinkhazard/static/js/index.js
@@ -0,0 +1,16 @@
+(function() {
+
+  var engine = new Bloodhound({
+    datumTokenizer: function(d) { return d.value; },
+    queryTokenizer: Bloodhound.tokenizers.whitespace,
+    local: [{ value: 'dog animal' }, { value: 'pig animal' }, { value: 'moose animal' }, { value: 'bird ' }]
+  });
+
+  engine.initialize();
+
+  $('#search-field').typeahead(null, {
+    name: 'animals',
+    source: engine.ttAdapter()
+  });
+
+})();
