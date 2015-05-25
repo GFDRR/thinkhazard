c33ad7ef65873ddacfe2d85c74cb900ee38fa3d5
Pierre GIRAUD <pierre.giraud@camptocamp.com>
2015-05-25 10:52:30 +0200

    Adding a basic OL map

--- thinkhazard/static/js/report.js
+++ thinkhazard/static/js/report.js
@@ -37,6 +37,22 @@
     }
   });
 
+  var map = new ol.Map({
+    target: 'map',
+    interactions: ol.interaction.defaults({
+      mouseWheelZoom: false
+    }),
+    layers: [
+      new ol.layer.Tile({
+        source: new ol.source.OSM()
+      })
+    ],
+    view: new ol.View({
+      center: [0, 0],
+      zoom: 5
+    })
+  });
+
   $('.drillup').on('click', function(e) {
     var code = $(this).attr('data-code');
     showDivision(code);
