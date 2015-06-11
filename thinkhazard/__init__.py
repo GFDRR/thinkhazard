51d683a8a7bff5df082c6b146f43193d74c48e4d
Pierre GIRAUD <pierre.giraud@camptocamp.com>
2015-06-11 14:37:08 +0200

    Fix typo in routing

--- thinkhazard/__init__.py
+++ thinkhazard/__init__.py
@@ -25,7 +25,7 @@ def main(global_config, **settings):
                            cache_max_age=86000, cachebust=True)
 
     config.add_route('index', '/')
-    config.add_route('report', '/report/{divisioncode:\d+}/{hazardtype\s+}')
+    config.add_route('report', '/report/{divisioncode:\d+}/{hazardtype:\s+}')
     config.add_route('report_overview', '/report/{divisioncode:\d+}')
     config.add_route('administrativedivision', '/administrativedivision')
     config.add_route('map', '/map.{format}')
