cfd4d21943dee807a79ab94a42bce75e4b637f49
Pierre GIRAUD <pierre.giraud@camptocamp.com>
2015-06-15 10:01:15 +0200

    Hover styling

--- thinkhazard/static/js/report.js
+++ thinkhazard/static/js/report.js
@@ -34,7 +34,35 @@
         width: 1
       }),
       text: new ol.style.Text({
-        text: 'toto',
+        text: feature.get('name'),
+        scale: 0.9,
+        stroke: new ol.style.Stroke({
+          color: 'rgba(255, 255, 255, 0.6)',
+          width: 5
+        }),
+        fill: new ol.style.Fill({
+          color: 'black'
+        })
+      })
+    })];
+  };
+
+  var styleHoverFn = function(feature) {
+    return [new ol.style.Style({
+      fill: new ol.style.Fill({
+        color: 'rgba(255, 120, 120, .3)'
+      }),
+      stroke: new ol.style.Stroke({
+        color: '#FF5555',
+        width: 1
+      }),
+      text: new ol.style.Text({
+        text: feature.get('name'),
+        scale: 1.2,
+        stroke: new ol.style.Stroke({
+          color: 'white',
+          width: 5
+        }),
         fill: new ol.style.Fill({
           color: 'black'
         })
@@ -53,4 +81,25 @@
   });
   map.addLayer(vector);
   window.source = vector.getSource();
+
+  var select = new ol.interaction.Select({
+    layers: [vector],
+    condition: ol.events.condition.pointerMove,
+    style: styleHoverFn
+  });
+  map.addInteraction(select);
+
+  // change mouse cursor when over division
+  map.on('pointermove', function(e) {
+    if (e.dragging) {
+      return;
+    }
+    var pixel = map.getEventPixel(e.originalEvent);
+    var hit = map.hasFeatureAtPixel(pixel, function(layer) {
+      if (layer == vector) {
+        return true;
+      }
+    });
+    map.getTargetElement().style.cursor = hit ? 'zoom-in' : '';
+  });
 })();
