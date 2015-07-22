fd3f22f0baa701d60935cef25aa1d26a5cd66882
Pierre GIRAUD <pierre.giraud@camptocamp.com>
2015-07-22 10:29:24 +0200

    Hazard icons are now using webfont in home page as well

--- thinkhazard/views/index.py
+++ thinkhazard/views/index.py
@@ -1,6 +1,11 @@
 from pyramid.view import view_config
 
+from ..models import (
+    DBSession,
+    HazardType)
+
 
 @view_config(route_name='index', renderer='templates/index.jinja2')
 def index(request):
-    return {}
+    hazard_types = DBSession.query(HazardType)
+    return {'hazards': hazard_types}
