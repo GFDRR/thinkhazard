d152d2d061798457f7526e6c6e5925c08a61ac72
Raphaël Pinson <raphael.pinson@camptocamp.com>
2015-08-06 13:31:25 +0200

    Detect package

--- .deploy.sh
+++ .deploy.sh
@@ -4,13 +4,12 @@ SSH_OPTS="-o StrictHostKeyChecking=no -i id_rsa"
 SSH_HOST="reprepro@pkg.camptocamp.net"
 SECTION="ci"
 DISTRO="jessie"
-# TODO: Detect/generate that
 PACKAGE_NAME="thinkhazard"
-PACKAGE="${PACKAGE_NAME}_0.0_amd64.deb"
 
 if [ -z $TRAVIS_TAG ]; then
   # TODO: pass $TRAVIS_TAG to make deb?
   make deb
+  PACKAGE=$(ls ${PACKAGE_NAME}_*.deb)
   scp $SSH_OPTS $PACKAGE $SSH_HOST:/var/packages/apt/incoming
   ssh $SSH_OPTS $SSH_HOST "/usr/bin/reprepro -b /var/packages/apt -S ${SECTION} -P optional includedeb ${DISTRO}/dev /var/packages/apt/incoming/${PACKAGE}"
 else
