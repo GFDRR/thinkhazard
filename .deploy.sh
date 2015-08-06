bdd6a5024ecd02903953923b22d5046a99c7b774
Éric Lemoine <eric.lemoine@gmail.com>
2015-08-06 15:22:48 +0200

    Run make deb as part of Travis' script step

--- .deploy.sh
+++ .deploy.sh
@@ -8,7 +8,6 @@ PACKAGE_NAME="thinkhazard"
 
 if [ -z $TRAVIS_TAG ]; then
   # TODO: pass $TRAVIS_TAG to make deb?
-  make deb
   PACKAGE=$(ls ${PACKAGE_NAME}_*.deb)
   scp $SSH_OPTS $PACKAGE $SSH_HOST:/var/packages/apt/incoming
   ssh $SSH_OPTS $SSH_HOST "/usr/bin/reprepro -b /var/packages/apt -S ${SECTION} -P optional includedeb ${DISTRO}/dev /var/packages/apt/incoming/${PACKAGE}"
