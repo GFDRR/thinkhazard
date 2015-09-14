121cf8424e59157b953853b8b95b1debd03c1e56
Pierre GIRAUD <pierre.giraud@camptocamp.com>
2015-09-14 15:05:11 +0200

    Ordering hazard types in home page as well

--- thinkhazard/views/index.py
+++ thinkhazard/views/index.py
@@ -7,5 +7,5 @@ from ..models import (
 
 @view_config(route_name='index', renderer='templates/index.jinja2')
 def index(request):
-    hazard_types = DBSession.query(HazardType)
+    hazard_types = DBSession.query(HazardType).order_by(HazardType.order)
     return {'hazards': hazard_types}
