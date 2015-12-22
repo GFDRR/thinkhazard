ce7fafa87d5ca1d88f681be2ea66b40a4c11fb3c
Pierre GIRAUD <pierre.giraud@camptocamp.com>
2015-12-22 15:04:26 +0100

    Add more documentations to database creation script

--- thinkhazard/scripts/createdb.sh
+++ thinkhazard/scripts/createdb.sh
@@ -5,25 +5,29 @@
 # - Drops the database and recreates it
 # - Creates the tables given the model (initialize)
 # - Populates the database with enum tables
+# - Populates the database with administrative divisions
 # - Creates an image file representing the DB model
 #
-# This is useful while working on the python model to make sure that it looks
-# like it really should in postgreSQL
+# This can be useful while working on the python model to make sure that it
+# looks like it really should in postgreSQL
+#
+# This script is intentionaly not exposed as target in the makefile.
 
-echo "restart"
+echo "Restarting PostgreSQL"
 sudo /etc/init.d/postgresql restart
-echo "drop"
+echo "Drop existing database"
 sudo -u postgres dropdb thinkhazard -U postgres
-echo "create"
+echo "Create new database"
 sudo -u postgres createdb thinkhazard -U postgres
-echo "postgis and schema"
+echo "Postgis and schema"
 sudo -u postgres psql -d thinkhazard -c 'CREATE EXTENSION postgis;'
 sudo -u postgres psql -d thinkhazard -c 'CREATE SCHEMA datamart;'
 sudo -u postgres psql -d thinkhazard -c 'GRANT ALL ON SCHEMA datamart TO "www-data";'
-echo "initialize"
+echo "Initialize"
 .build/venv/bin/initialize_thinkhazard_db development.ini
 .build/venv/bin/nosetests
 
+echo "Creating database diagram"
 cd /tmp
 sudo -u postgres postgresql_autodoc -d thinkhazard
 dot -Tpng thinkhazard.dot > thinkhazard.png
