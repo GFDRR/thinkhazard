cd3eaf1df40285bc68f87bbda2553c7c1e37c934
arnaud.morvan@camptocamp.com <arnaud.morvan@camptocamp.com>
2016-03-25 11:24:45 +0100

    Add sitemap view

---
+++ thinkhazard/tests/views/test_sitemap.py
@@ -0,0 +1,29 @@
+# -*- coding: utf-8 -*-
+#
+# Copyright (C) 2015-2016 by the GFDRR / World Bank
+#
+# This file is part of ThinkHazard.
+#
+# ThinkHazard is free software: you can redistribute it and/or modify it under
+# the terms of the GNU General Public License as published by the Free
+# Software Foundation, either version 3 of the License, or (at your option)
+# any later version.
+#
+# ThinkHazard is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+# FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+# more details.
+#
+# You should have received a copy of the GNU General Public License along with
+# ThinkHazard.  If not, see <http://www.gnu.org/licenses/>.
+
+from . import BaseTestCase
+
+
+class TestSitemapFunction(BaseTestCase):
+
+    def test_sitemap(self):
+        resp = self.testapp.get('/sitemap.xml', status=200)
+        urls = resp.xml.findall(
+            '{http://www.sitemaps.org/schemas/sitemap/0.9}url')
+        self.assertEqual(len(urls), 7)
