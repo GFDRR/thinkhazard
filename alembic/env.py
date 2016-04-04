b37bfeadd9ac29a0d176355932d98cbfa71a4a5d
Pierre GIRAUD <pierre.giraud@camptocamp.com>
2016-04-04 09:16:54 +0200

    load local settings with name of application as parameter

--- alembic/env.py
+++ alembic/env.py
@@ -2,6 +2,9 @@ from __future__ import with_statement
 from alembic import context
 from sqlalchemy import engine_from_config, pool
 from logging.config import fileConfig
+import ConfigParser
+import re
+import os
 from thinkhazard.settings import load_local_settings
 
 # this is the Alembic Config object, which provides
