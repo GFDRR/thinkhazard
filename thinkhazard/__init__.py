5b5f8e64db237205fe7f2fe0db4952df117c0c8d
arnaud.morvan@camptocamp.com <arnaud.morvan@camptocamp.com>
2016-04-08 12:02:58 +0200

    Fix error with pdf_archive_path on travis

--- thinkhazard/__init__.py
+++ thinkhazard/__init__.py
@@ -85,8 +85,6 @@ def main(global_config, **settings):
 
     config.add_renderer('geojson', GeoJSON())
 
-    init_pdf_archive_directory(settings.get('pdf_archive_path'))
-
     scan_ignore = ['thinkhazard.tests']
     if settings['appname'] != 'public':
         scan_ignore.append('thinkhazard.views.sitemap')
@@ -139,10 +137,3 @@ def add_public_routes(config):
     config.add_route('pdf_cover', '/pdf_cover/{divisioncode:\d+}')
     config.add_route('pdf_about', '/pdf_about')
     config.add_route('data_source', '/data_source/{hazardset}')
-
-
-def init_pdf_archive_directory(pdf_archive_path):
-    """Make sure that the directory used as report archive exists.
-    """
-    if not os.path.exists(pdf_archive_path):
-        os.makedirs(pdf_archive_path)
