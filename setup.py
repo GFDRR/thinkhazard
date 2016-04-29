8aaf02a23f4d64291d1f928307a2d0b571155592
Pierre GIRAUD <pierre.giraud@camptocamp.com>
2016-04-29 14:56:29 +0200

    Bumping version 1.4-dev

--- setup.py
+++ setup.py
@@ -50,7 +50,7 @@ requires = [
     ]
 
 setup(name='thinkhazard',
-      version='1.3-dev',
+      version='1.4-dev',
       description='thinkhazard',
       long_description=README + '\n\n' + CHANGES,
       classifiers=[
