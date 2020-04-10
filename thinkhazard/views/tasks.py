9760f229e1903c5cf1041fa3f233b9bde371d42b
Antoine Abt <antoine@abt.im>
2020-04-10 15:41:04 +0200

    tmp: import admin div

--- thinkhazard/views/tasks.py
+++ thinkhazard/views/tasks.py
@@ -1,8 +1,25 @@
 from pyramid_celery import celery_app as app
+import importlib
+imp = importlib.import_module('thinkhazard.processing.import')
+import os
 import time
+
+INI_FILE = os.environ['INI_FILE']
 
 @app.task
 def publish():
     print('start publish')
     time.sleep(10)
     print('end publish')
+
+@app.task
+def transifex_fetch():
+    print('start pull/import from transifex')
+    print('end pull/import from transifex')
+
+@app.task
+def admindivs():
+    print('start import administrative divisions')
+    print(INI_FILE)
+    imp.AdministrativeDivisionsImporter.run((INI_FILE,))
+    print('end import administrative divisions')
