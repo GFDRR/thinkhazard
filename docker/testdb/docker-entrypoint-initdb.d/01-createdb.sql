2c4032d3f1d70c992052537bf491968cffccde33
arnaud.morvan@camptocamp.com <arnaud.morvan@camptocamp.com>
2021-10-19 14:41:43 +0200

    Separe and fix tests composition

--- docker/testdb/docker-entrypoint-initdb.d/01-createdb.sql
+++ docker/testdb/docker-entrypoint-initdb.d/01-createdb.sql
@@ -1,16 +1,7 @@
-CREATE USER thinkhazard;
-
-CREATE DATABASE thinkhazard OWNER thinkhazard;
-\connect thinkhazard;
 CREATE EXTENSION postgis;
 CREATE EXTENSION unaccent;
 
 CREATE DATABASE thinkhazard_admin OWNER thinkhazard;
 \connect thinkhazard_admin;
-CREATE EXTENSION postgis;
-CREATE EXTENSION unaccent;
-
-CREATE DATABASE thinkhazard_test OWNER thinkhazard;
-\connect thinkhazard_test;
 CREATE EXTENSION postgis;
 CREATE EXTENSION unaccent;
