8e18ee45c625a72415fb66cd6c634439b45ae1f5
arnaud.morvan@camptocamp.com <arnaud.morvan@camptocamp.com>
2026-02-24 14:25:08 +0100

    Disable cache on publication date as it does not take care of code revision

--- thinkhazard/__init__.py
+++ thinkhazard/__init__.py
@@ -54,7 +54,7 @@ def main(global_config, **settings):
     config.include("thinkhazard.session")
 
     config.add_tween("thinkhazard.tweens.set_secure_headers", over=MAIN)
-    config.add_tween("thinkhazard.tweens.notmodified_tween_factory", over=MAIN)
+    config.add_tween("thinkhazard.tweens.no_cache_tween_factory", over=MAIN)
 
     config.add_static_view("static", "thinkhazard:static", cache_max_age=3600)
     config.override_asset(
